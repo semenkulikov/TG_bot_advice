e93db9f24f463bd8b33aebb20a88a1773f68d318
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-01-28 22:32:16 +0300

    Added main file and configs

---
+++ config_data/config.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+if not find_dotenv():
+    exit('Переменные окружения не загружены, т.к отсутствует файл .env')
+else:
+    load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+DEFAULT_COMMANDS = (
+    ('start', "Запустить бота"),
+    ('help', "Вывести справку")
+)
+ADMIN_COMMANDS = (
+    ("admin_panel", "Админка"),
+)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PATH_TO_PYTHON = os.path.normpath(os.path.join(BASE_DIR, "venv/Scripts/python.exe"))
+ADMIN_ID = os.getenv('ADMIN_ID')
+ALLOWED_USERS = [int(ADMIN_ID),]
