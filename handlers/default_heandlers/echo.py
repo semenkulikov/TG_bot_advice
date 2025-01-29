ad228cb8fff2ac08fc270af1c836494016c3c444
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-01-29 17:53:08 +0300

    Added default reply buttons

--- handlers/default_heandlers/echo.py
+++ handlers/default_heandlers/echo.py
@@ -1,11 +1,19 @@
 from telebot.types import Message
 from loader import bot
+from handlers.custom_heandlers.reservation_handlers import advice_handler
+from handlers.custom_heandlers.user_handlers import my_advices_handler, faq_handler
 
 
 # Эхо хендлер, куда летят текстовые сообщения без указанного состояния
 
 @bot.message_handler(state=None)
 def bot_echo(message: Message):
-    bot.reply_to(message, f"Введите любую команду из меню, чтобы я начал работать\n"
-                          f"Либо выберите одну из кнопок, которые я вам прислал")
-
+    if message.text == "Записаться":
+        advice_handler(message)
+    elif message.text == "Мои записи":
+        my_advices_handler(message)
+    elif message.text == "Частые вопросы":
+        faq_handler(message)
+    else:
+        bot.reply_to(message, f"Введите любую команду из меню, чтобы я начал работать\n"
+                              f"Либо выберите одну из кнопок, которые я вам прислал")
--- keyboards/reply/handlers_reply.py
+++ keyboards/reply/handlers_reply.py
@@ -2,9 +2,8 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 
 def handlers_reply() -> ReplyKeyboardMarkup:
-    keyboard = ReplyKeyboardMarkup(True, True, row_width=2,
-                                   input_field_placeholder="Нажмите на нужную кнопку...")
-    keyboard.add(KeyboardButton('Test button'), KeyboardButton('Test button 2'))
-    keyboard.add(KeyboardButton('Test 3'))
-    keyboard.add(KeyboardButton('Test 4'))
+    keyboard = ReplyKeyboardMarkup(True, False, row_width=2,
+                                   input_field_placeholder="Выберите нужную кнопку...")
+    keyboard.add(KeyboardButton('Записаться'), KeyboardButton('Мои записи'))
+    keyboard.add(KeyboardButton('Частые вопросы'))
     return keyboard
